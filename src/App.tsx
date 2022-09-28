8b1ad1861f368ce7df3662a8b7cfe1e5949a2464
Bruno Viveiros <bruno.viveiros@codeminer42.com>
2022-09-28 16:38:54 -0300

    chore: setup base theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,15 @@
-import { createGlobalStyle } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 
-const GlobalStyle = createGlobalStyle`
-html {
-  box-sizing: border-box;
-  font-size: 16px;
-}
-
-*, *:before, *:after {
-  box-sizing: inherit;
-}
-
-body, h1, h2, h3, h4, h5, h6, p, ol, ul {
-  margin: 0;
-  padding: 0;
-  font-weight: normal;
-}
-
-ol, ul {
-  list-style: none;
-}
-
-img {
-  max-width: 100%;
-  height: auto;
-}
-`;
+import ResetStyle from './styles/ResetStyle';
+import { theme } from './styles/theme';
 
 const App = () => {
   return (
     <>
-      <GlobalStyle />
-      <h1>Hello World</h1>
+      <ThemeProvider theme={theme}>
+        <ResetStyle />
+        <h1>Hello World</h1>
+      </ThemeProvider>
     </>
   );
 };
---
+++ src/styles/theme.tsx
@@ -0,0 +1,33 @@
+const theme = {
+  colors: {
+    type: {
+      fairy: '#E69EAC',
+      ice: '#9AD6DF',
+      grass: '#74CB48',
+      steal: '#B7B9D0',
+      rock: '#B69E31',
+      dark: '#75574C',
+      psychic: '#FB5584',
+      water: '#6493EB',
+      ghost: '#70559B',
+      ground: '#DEC16B',
+      dragon: '#7037FF',
+      electric: '#F9CF30',
+      fire: '#F57D31',
+      bug: '#A7B723',
+      poison: '#A43E9E',
+      flying: '#A891EC',
+      fighting: '#C12239',
+      normal: '#AAA67F',
+    },
+    primarybackground: '#F7F7F7',
+    secondaryBackground: '#FFFFFF',
+    primaryFontColor: '#212121',
+    secondaryFontColor: '#FFFFFF',
+    border: '#E0E0E0',
+    placeholder: '#666666',
+  },
+};
+
+export { theme };
+export type Theme = typeof theme;
